05825f9c59431cfbcfebe06d0a9fbe509b079985
Nick Jocius <capacity512@yahoo.com>
2021-07-30 19:37:41 -0500

    created full testing bed

---
+++ main.h
@@ -0,0 +1,27 @@
+/*********************************
+Name: Nick Jocius
+Binary Search Tree C++
+File: main.h
+Purpose: Create a binary search tree
+object that will work with the same struct
+Data and struct Node from previous assignments.
+**********************************/
+
+#ifndef BINTREE_MAIN_H
+#define BINTREE_MAIN_H
+
+#include <iostream> /* cout, endl */
+#include <stdlib.h> /* srand      */
+#include <time.h>   /* time       */
+#include <string>   /* string     */
+#include "bintree.h"
+#include "functions.h"
+
+using std::cout;
+using std::endl;
+using std::string;
+
+#define TESTDATA1 7
+#define TESTDATA2 6
+
+#endif /* BINTREE_MAIN_H */
